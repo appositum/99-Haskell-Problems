ec376ab5dfd7f62767d48793d9e21eaba6cfca7f
appositum <appositum@protonmail.com>
2018-07-26 21:46:58 -0300

    Better solutions

--- 1_lists/8_problem_8.hs
+++ 1_lists/8_problem_8.hs
@@ -4,5 +4,4 @@ compress :: String -> String
 compress = concatMap nub . group
 
 main :: IO ()
-main = do
-    print $ compress "aaaabccaadeeee" -- "abcade"
+main = print $ compress "aaaabccaadeeee" -- "abcade"
