a6aeb6af487b17f1b0e38d5475fe5d1d5b4f14ab
appositum <appositum@protonmail.com>
2018-07-26 21:46:58 -0300

    Better solutions

--- 1_lists/9_problem_9.hs
+++ 1_lists/9_problem_9.hs
@@ -4,5 +4,4 @@ pack :: [Char] -> [String]
 pack = groupBy (==)
 
 main :: IO ()
-main = do
-    print $ pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] -- ["aaaa","b","cc","aa","d","eeee"]
+main = print $ pack "aaaabccaadeeee" -- ["aaaa","b","cc","aa","d","eeee"]
